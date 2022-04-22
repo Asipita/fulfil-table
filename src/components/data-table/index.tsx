1fbe54f75e03f35186766b3ca4042d6b84067e8c
Shuaib Abdulgafar <ashuaib@afexnigeria.com>
2022-04-22 08:56:47 +0100

    right-align numeric fields

--- src/components/data-table/index.tsx
+++ src/components/data-table/index.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from "react";
-
+import { ReactNode, useMemo } from "react";
+// import clsx from "clsx";
 interface IDataTableProps {
   columns: Array<IColumnProps>;
   rows: Array<IRowProps>;
@@ -23,20 +23,36 @@ interface IRowProps {
 export function DataTable(props: IDataTableProps) {
   const { columns, rows, onRowClick, onSelectionChange } = props;
 
+  function isNumericRow(idx: number): boolean {
+    return !!columns[idx+1].isNumeric;
+  }
+
   return (
     <section className="border rounded-lg">
       <table className="w-full">
         <thead>
-          {columns.map((column: IColumnProps) => (
-            <th className="p-4 text-left border">{column.label}</th>
+          {columns.map(({ label, isNumeric, id }: IColumnProps) => (
+            <th key={id} className="p-4 border" align={isNumeric ? "right" : "left"}>
+              {label}
+            </th>
           ))}
         </thead>
         <tbody>
           {rows.map((row: IRowProps) => (
             <tr key={row.id} onClick={onRowClick} className="border p-4">
-              <input type="checkbox" onChange={onSelectionChange} className="p-4 m-4 place-self-center"/>
-              {Object.values(row).map((entry) => (
-                <td className="p-4 border">{entry}</td>
+              <input
+                type="checkbox"
+                onChange={onSelectionChange}
+                className="p-4 m-4 place-self-center"
+              />
+              {Object.values(row).map((entry, idx) => (
+                <td
+                  className="p-4 border"
+                  align={isNumericRow(idx) ? "right" : "left"}
+                  key={idx}
+                >
+                  {entry}
+                </td>
               ))}
             </tr>
           ))}
